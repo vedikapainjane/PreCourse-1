09dd339af06003bbd43bab4aeb506bb3bc9c0f48
Vedika Vishwanath Painjane <vp2312@rit.edu>
2025-01-30 20:46:28 -0500

    Exercise 1 completed

--- Exercise_1.java
+++ Exercise_1.java
@@ -1,3 +1,11 @@
+// Exercise_1 : Implement Stack using Array.
+// Time Complexity : O(1)
+// Space Complexity : O(n) where n is the number of items in the stack.
+// Did this code successfully run on Leetcode : 
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
 class Stack { 
     //Please read sample.java file before starting.
   //Kindly include Time and Space complexity at top of each file
@@ -7,30 +15,59 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
-        int a;
+        //if stack is empty, then the top will be less than 0, 
+        // where 0 will be the first index of the array.
+        return (top < 0);
     } 
 
     Stack() 
     { 
         //Initialize your constructor 
+        // since first element will go on the 0th index of the array.
+        top = -1;
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+        if (top >= MAX - 1){
+            System.out.println("Stack Overflow!");
+            return false;
+        }
+        else{
+            a[++top] = x;
+            System.out.println(x + " pushed onto the stack");
+            return true;
+        }
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
-        //Write your code here
+        if (top < 0){
+            System.out.println("Stack Underflow!");
+            return 0;
+        }
+        else{
+            int x = a[top--];
+            System.out.println(x + " popped put of the stack");
+            return x;
+        }
     } 
   
     int peek() 
     { 
-        //Write your code here
+        if (top < 0){
+            System.out.println("Stack Underflow!");
+            return 0;
+        }
+        else{
+            int x = a[top];
+            System.out.println(x + " is top of the stack");
+            return x;
+        }
+
     } 
 } 
   
